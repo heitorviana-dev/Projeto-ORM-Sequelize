85073acce88fca683f87177e1efdc73128b38ceb
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-06-20 09:25:02 -0300

    Corrigindo erro import/export no eslint

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.get('/teste', (req, res) => {
-//   res
-//     .status(200)
-//     .send({ mensagem: 'boas-vindas à API' });
-// });
-
-// module.exports = app;
-
 import express from 'express';
 
 class App{
