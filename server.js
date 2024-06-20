4c15577aa0a74de9eb296c6c55059508caa0a16f
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-06-20 10:54:02 -0300

    Configurando o Sequelize

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ import app from './src/app.js';
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log('servidor escutando!');
+  console.log();
+  console.log('Servidor escutando na porta 3000');
+  console.log('Acesse http://localhost:3000');
 });
